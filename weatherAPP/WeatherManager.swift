e7f35731cef4555fd451dbda8fe7689df8917e53
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-03-24 15:19:43 -0400

    completionhandler, request and fetch api data

--- weatherAPP/WeatherManager.swift
+++ weatherAPP/WeatherManager.swift
@@ -10,10 +10,35 @@ import Foundation
 
 struct WeatherManager {
     let weatherURL =
-    "http://api.openweathermap.org/data/2.5/weather?appid=d418aaba4e9cf886d4e98d9b56a738b8&units=metric"
+    "https://api.openweathermap.org/data/2.5/weather?appid=d418aaba4e9cf886d4e98d9b56a738b8&units=metric"
     
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+        
+    }
+    
+    func performRequest(urlString: String){
+        //Create a Url
+        if let url = URL(string: urlString){
+            // create a UrlSession
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error:Error?)  {
+        if error != nil{
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
